38eafef55d0d77f422ccd0a72b35ae466355b065
Fox,Mathijs M <m.fox@student.fontys.nl>
2020-03-03 11:10:24 +0100

    Initial commit

---
+++ src/main/java/Application/ExpploreBackEndApplication.java
@@ -0,0 +1,13 @@
+package Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpploreBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpploreBackEndApplication.class, args);
+	}
+
+}
