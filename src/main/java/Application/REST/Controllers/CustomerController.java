55f9d15cc8a370cd8b5def7038332871ce00324e
Fox,Mathijs M <m.fox@student.fontys.nl>
2020-04-30 14:20:04 +0200

    added customer Controller

---
+++ src/main/java/Application/REST/Controllers/CustomerController.java
@@ -0,0 +1,11 @@
+package Application.REST.Controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+}
